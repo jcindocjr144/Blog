9efca97da8a67d4eda17ce92ae676180f4f31bc7
jcindocjr144 <128598274+jcindocjr144@users.noreply.github.com>
2023-12-09 11:04:04 +0800

    Update blog.js

--- JS/blog.js
+++ JS/blog.js
@@ -3,8 +3,3 @@ function museum(){
   document.getElementById("home").style.display="none";
   document.getElementById("10k").style.display="none";
 }
-function 10k(){
-  document.getElementById("10k").style.display="block";
-  document.getElementById("home").style.display="none";
-  document.getElementById("museum").style.display="none";
-}
