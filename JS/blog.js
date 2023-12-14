563be21cbb17d099cde2c402aad7ea5ede059d98
jcindocjr144 <128598274+jcindocjr144@users.noreply.github.com>
2023-12-15 07:37:55 +0800

    Update blog.js

--- JS/blog.js
+++ JS/blog.js
@@ -5,12 +5,12 @@ function home(){
 }
 function museum(){
   document.getElementById("museum").style.display="block";
+  document.getElementById("centennial").style.display="none";
   document.getElementById("roses").style.display="none";
   document.getElementById("home").style.display="none";
   document.getElementById("about").style.display="none";
   document.getElementById("contact").style.display="none";
   document.getElementById("footer").style.display="none";
-  document.getElementById("centennial").style.display="none";
 }
 function roses(){
   document.getElementById("roses").style.display="block";
