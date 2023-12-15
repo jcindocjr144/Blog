b8a70e3a29f9c4617345a9b13c4d4cf424b640aa
jcindocjr144 <128598274+jcindocjr144@users.noreply.github.com>
2023-12-15 08:14:20 +0800

    Update blog.js

--- JS/blog.js
+++ JS/blog.js
@@ -6,15 +6,18 @@ function home(){
 function museum(){
   document.getElementById("museum").style.display="block";
   document.getElementById("centennial").style.display="none";
+  document.getElementById("bakasihan").style.display="none";
   document.getElementById("roses").style.display="none";
   document.getElementById("home").style.display="none";
   document.getElementById("about").style.display="none";
   document.getElementById("contact").style.display="none";
   document.getElementById("footer").style.display="none";
+  document.getElementById("bakasihan").style.display="none";
 }
 function roses(){
   document.getElementById("roses").style.display="block";
   document.getElementById("museum").style.display="none";
+  document.getElementById("bakasihan").style.display="none";
   document.getElementById("home").style.display="none";
   document.getElementById("about").style.display="none";
   document.getElementById("contact").style.display="none";
@@ -24,8 +27,20 @@ function roses(){
 function next(){
   document.getElementById("centennial").style.display="block";
   document.getElementById("roses").style.display="none";
+  document.getElementById("bakasihan").style.display="none";
 }
 function previous(){
   document.getElementById("centennial").style.display="none";
   document.getElementById("roses").style.display="block";
+  document.getElementById("bakasihan").style.display="none";
+}
+function next1(){
+  document.getElementById("bakasihan").style.display="block";
+  document.getElementById("centennial").style.display="none";
+  document.getElementById("roses").style.display="none";
+}
+function previous1(){
+  document.getElementById("centennial").style.display="block";
+  document.getElementById("roses").style.display="none";
+  document.getElementById("bakasihan").style.display="none";
 }
