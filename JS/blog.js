9983e2559c4df93a5475dd589d9ae80119e6af09
jcindocjr144 <128598274+jcindocjr144@users.noreply.github.com>
2023-12-15 08:38:58 +0800

    Update blog.js

--- JS/blog.js
+++ JS/blog.js
@@ -1,21 +1,20 @@
 function home(){
   document.getElementById("home").style.display="block";
+  document.getElementById("roses").style.display="none";
+  document.getElementById("museum").style.display="none";
 }
 function museum(){
   document.getElementById("museum").style.display="block";
   document.getElementById("centennial").style.display="none";
-  document.getElementById("bakasihan").style.display="none";
   document.getElementById("roses").style.display="none";
   document.getElementById("home").style.display="none";
   document.getElementById("about").style.display="none";
   document.getElementById("contact").style.display="none";
   document.getElementById("footer").style.display="none";
-  document.getElementById("bakasihan").style.display="none";
 }
 function roses(){
   document.getElementById("roses").style.display="block";
   document.getElementById("museum").style.display="none";
-  document.getElementById("bakasihan").style.display="none";
   document.getElementById("home").style.display="none";
   document.getElementById("about").style.display="none";
   document.getElementById("contact").style.display="none";
@@ -25,20 +24,8 @@ function roses(){
 function next(){
   document.getElementById("centennial").style.display="block";
   document.getElementById("roses").style.display="none";
-  document.getElementById("bakasihan").style.display="none";
 }
 function previous(){
   document.getElementById("centennial").style.display="none";
   document.getElementById("roses").style.display="block";
-  document.getElementById("bakasihan").style.display="none";
-}
-function next1(){
-  document.getElementById("bakasihan").style.display="block";
-  document.getElementById("centennial").style.display="none";
-  document.getElementById("roses").style.display="none";
-}
-function previous1(){
-  document.getElementById("centennial").style.display="block";
-  document.getElementById("roses").style.display="none";
-  document.getElementById("bakasihan").style.display="none";
 }
